070fcf7353d12f9febd5d554055efa6074f24402
Jingxi Xu <zealliant@outlook.com>
2018-09-27 18:22:37 -0400

    update map and debug

--- src/create_map.py
+++ src/create_map.py
@@ -36,7 +36,6 @@ def load_goal(goal_path):
 		line = f.readline()
 		goal = list(map(int, line.strip().split(' ')))
 	return goal
-
 """ 
 ------------------------------------------
 (0, 0) is at the center of the map;
@@ -50,9 +49,9 @@ def map2img(ob):
 	""" transform an obstacle in map frame to img frame """
 	ob_tranformed = []
 	t = np.array([[1, 0, 0, 300], 
-				[0, -1, 0, 300], 
-				[0, 0, -1, 0],
-				[0, 0, 0, 1]])
+					[0, -1, 0, 300], 
+					[0, 0, -1, 0],
+					[0, 0, 0, 1]])
 	for p in ob:
 		p = np.array(p) # in case it is not already numpy array
 		p = np.hstack((p, [0, 1]))
@@ -64,9 +63,9 @@ def img2map(ob):
 	""" transform an obstacle in img frame to map frame """
 	ob_tranformed = []
 	t = np.array([[1, 0, 0, -300], 
-				[0, -1, 0, 300],
-				[0, 0, -1, 0],
-				[0, 0, 0, 1]])
+					[0, -1, 0, 300],
+					[0, 0, -1, 0],
+					[0, 0, 0, 1]])
 	for p in ob:
 		p = np.array(p) # in case it is not already numpy array
 		p = np.hstack((p, [0, 1]))
@@ -76,12 +75,12 @@ def img2map(ob):
 
 if __name__ == "__main__":
 
-	# create a black image
+	# Create a black image
 	img = np.full((600,1200,3), 255, np.uint8)
 
-	# load obstacles and goal position
 	obstacles = load_obstacles("../data/world_obstacles.txt")
 	goal = load_goal("../data/goal.txt")
+	start = [0, 0]
 
 	# draw obstacles
 	for ob in obstacles:
@@ -90,11 +89,15 @@ if __name__ == "__main__":
 		# print(ob)
 		cv2.fillConvexPoly(img, ob.reshape(-1, 1, 2), (255,255,0))
 
-	# draw goal as a filled circle
+	# draw start and goal point
 	goal = tuple(map2img([goal])[0])
+	start = tuple(map2img([start])[0])
 	cv2.circle(img, goal, 7, (100, 0, 0), -1)
+	cv2.circle(img, start, 7, (0, 0, 100), -1)
 
-	# show the created image
+	# import pdb; pdb.set_trace()
+	# cv2.polylines(img,[pts],True,(0,255,255), thickness=1)
+
 	cv2.imshow( "Display window", img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
